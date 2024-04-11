e79048a51aba588c7defc56d38e45f6f10684b08
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-04-11 11:07:31 +0200

    travail sur les modals, incrémentation de la variable i pour créer un id numéroté pour chaque modal

--- assets/scripts/recettes.js
+++ assets/scripts/recettes.js
@@ -7,7 +7,10 @@ $(document).ready(function(){
         const recettes = data.recettes;
 
         let listeRecettesHtml = '';
+        let i = 0
         recettes.forEach((recette, index) => {
+            i++
+            console.log(i)
             let recetteIngredients = recette.ingredients
             let ingredientsHtml = ''
             let quantiteHtml = ''
@@ -19,21 +22,18 @@ $(document).ready(function(){
             if (index % 3 === 0) {
                 listeRecettesHtml += '<div name="cartes" id="'+pageIndex+'">';
             }
-            listeRecettesHtml += '<div class="card"><div class="card-image"><img src="'+recette.img+'"><span class="card-title">'+recette.nom+'</span><a class="btn-floating halfway-fab waves-effect waves-light red modal-trigger" href="#modal1"><i class="material-icons">add</i></a></div><div class="card-content"><ul>'+ingredientsHtml+'</ul><ul>'+quantiteHtml+'</ul></div></div>';
+
+            listeRecettesHtml += '<div class="card"><div class="card-image"><img src="'+recette.img+'"><span class="card-title">'+recette.nom+'</span><a class="btn-floating halfway-fab waves-effect waves-light red modal-trigger" href="#recette'+i+'"><i class="material-icons">add</i></a></div><div class="card-content"><ul>'+ingredientsHtml+'</ul><ul>'+quantiteHtml+'</ul></div></div><div id="recette'+i+'" class="modal"><div class="modal-content"><h4>'+recette.nom+'</h4><p>A bunch of text</p></div><div class="modal-footer"><a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a></div></div>';
             if ((index + 1) % 3 === 0 || index === recettes.length - 1) {
                 listeRecettesHtml += '</div>';
             }
         });
         $('.box').html(listeRecettesHtml);
+
         //On force le visiteur a atterir sur la page 1 et à n'afficher que 3 cartes
         window.location.href="#page1"
         $('#page1').siblings('div').hide()
         $('#page1-btn').parent().addClass('active')
-
-        //On ajoute une modal au clic du bouton de la carte
-        $('.card a').after('<div id="modal1" class="modal"><div class="modal-content"><h4>Modal Header</h4><p>A bunch of text</p></div><div class="modal-footer"><a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a></div></div>')
-
-
 
         //Ici on fait apparaître 3 cartes en fonction de la page et on fait disparaître les autres
         //previous
