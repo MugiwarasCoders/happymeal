dd54e36ffec881c92cff19e38eb5f746d8b2c3df
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-04-15 09:54:41 +0200

    modifs conflits localStorage

--- assets/scripts/favoris.js
+++ assets/scripts/favoris.js
@@ -16,17 +16,33 @@ $(document).ready(function(){
 
         // Récupération de la liste des favoris depuis le stockage local (localStorage)
         let favListe = localStorage.getItem('favs');
+        let favListeIndex = localStorage.getItem('Recettes favorites:')
+        console.log(favListeIndex)
         // Conversion de la chaîne en tableau en utilisant la virgule comme séparateur
         favListe = favListe.split(',');
         console.log(favListe)
         
         // Vérification si la liste des favoris est vide
-        if (favListe == ''){
+        if (favListe == '' && favListeIndex == ''){
             $('.collection').html("Vous n'avez ajouté aucun favori pour l'instant")
         }
 
         // Parcours de la liste des favoris
         $(favListe).each(function(index, favName){
+            // Vérification si le favori existe dans la liste des recettes
+            let favoriExiste = allRecettes.some(recette => recette.nom === favName);
+            if (favoriExiste){
+                // Récupération de la recette correspondante
+                let recette = allRecettes.find(recette => recette.nom === favName);
+                // Construction de l'élément HTML pour afficher le favori
+                let favHtml = '<li class="collection-item"><p>'+recette.nom+'</p><button class="supItem"><img src="../img/bin.png"></button><button class="voir">Voir la recette</button></li>';
+                // Ajout de l'élément HTML à la liste des favoris
+                $('.collection').append(favHtml);
+            }
+        });
+
+        // Parcours de la liste des favoris
+        $(favListeIndex).each(function(index, favName){
             // Vérification si le favori existe dans la liste des recettes
             let favoriExiste = allRecettes.some(recette => recette.nom === favName);
             if (favoriExiste){
